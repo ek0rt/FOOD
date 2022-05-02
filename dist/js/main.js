30640c4dcca1d2af4ad7208a5055605173724cf8
Kort <de.kort@yahoo.de>
2022-05-02 14:06:27 +0300

    tabs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,42 @@
-console.log('d')
-console.log('d')
-console.log('d')
-console.log('d')
+let tabcontent = document.querySelectorAll('.tabcontent');
+let tabItems = document.querySelectorAll('.tabheader__item');
+let tabParent = document.querySelector('.tabheader__items');
+
+function hideContent() {
+    tabcontent.forEach(item => {
+        item.classList.add('hide')
+        item.classList.remove('show', 'fade')
+    })
+
+    tabItems.forEach(item => {
+        item.classList.remove('tabheader__item_active');
+    })
+
+};
+
+function showContent(i = 0) {
+    tabcontent[i].classList.add('show', 'fade')
+    tabcontent[i].classList.remove('hide')
+    tabItems[i].classList.add('tabheader__item_active')
+
+};
+
+tabParent.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if(target.classList.contains('tabheader__item')) {
+        tabItems.forEach((item, i) => {
+            if(target == item) {
+                hideContent()
+                showContent(i)
+            }
+        })
+    }
+
+})
+
+hideContent()
+showContent()
+
+
+
