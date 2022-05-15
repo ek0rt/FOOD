bd5d678231f0e49f5d757963116cea28166c785c
Kort <de.kort@yahoo.de>
2022-05-16 02:52:22 +0300

    com

--- src/js/main.js
+++ src/js/main.js
@@ -149,6 +149,7 @@ function getDifference(endtime) {
 
   // Используем классы для карточек
 
+
   class getCards {
     constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
@@ -215,6 +216,61 @@ new getCards(
     ".menu .container",
     'menu__item',
 ).render();
+
+// Forms
+
+const forms = document.querySelectorAll('form');
+const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+};
+
+forms.forEach(item => {
+    postData(item);
+});
+
+function postData(form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        statusMessage.textContent = message.loading;
+        form.appendChild(statusMessage);
+    
+        const request = new XMLHttpRequest();
+        request.open('POST', '../server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        const formData = new FormData(form);
+
+        const object = {};
+        formData.forEach(function(value, key){
+            object[key] = value;
+        });
+        const json = JSON.stringify(object);
+
+        request.send(json);
+
+        request.addEventListener('load', () => {
+            if (request.status === 200) {
+                console.log(request.response);
+                statusMessage.textContent = message.success;
+                form.reset();
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 2000);
+            } else {
+                statusMessage.textContent = message.failure;
+            }
+        });
+    });
+}
+
+
+
+
+
 
  
 getElements('2022-05-11T00:43:06');
