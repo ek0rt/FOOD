be725ca31c9ce83abe7aae3f70f7d33cb338a81b
Kort <de.kort@yahoo.de>
2022-05-20 00:47:13 +0300

    con

--- src/js/main.js
+++ src/js/main.js
@@ -240,9 +240,7 @@ function postData(form) {
         `;
         form.insertAdjacentElement('afterend', statusMessage);
     
-        const request = new XMLHttpRequest();
-        request.open('POST', '../server.php');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+     
         const formData = new FormData(form);
 
         const object = {};
@@ -251,18 +249,31 @@ function postData(form) {
         });
         const json = JSON.stringify(object);
 
-        request.send(json);
-
-        request.addEventListener('load', () => {
-            if (request.status === 200) {
-                console.log(request.response);
-                showThanksModal(message.success)
-                form.reset();
-                    statusMessage.remove();
-            } else {
+        fetch('../server.php', {
+          method: 'POST',
+          headers: {'Content-type': 'application/json; charset=utf-8'},
+          body: JSON.stringify(object),
+        }).then(data => data.text())
+        .then(data => {
+            console.log(data);
+            showThanksModal(message.success)
+            statusMessage.remove();
+        }).catch(() => {
               showThanksModal(message.failure);
-            }
-        });
+        }).finally(() => {
+          form.reset();
+        })
+
+        // request.addEventListener('load', () => {
+        //     if (request.status === 200) {
+        //         console.log(request.response);
+        //         showThanksModal(message.success)
+        //         form.reset();
+        //         statusMessage.remove();
+        //     } else {
+        //       showThanksModal(message.failure);
+        //     }
+        // });
     });
 }
 
@@ -292,6 +303,8 @@ function showThanksModal(message) {
   }, 3000);
 
 }
+
+console.log('++++')
 
 getElements('2022-05-11T00:43:06');
 hideContent()
