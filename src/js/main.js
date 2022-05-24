7101e7635f674bc7a8b1ee0efbaa39b9ad8ef515
Kort <de.kort@yahoo.de>
2022-05-25 01:04:57 +0300

    com

--- src/js/main.js
+++ src/js/main.js
@@ -202,6 +202,37 @@ function getDifference(endtime) {
   })
 
 
+  // getInfo('http://localhost:3000/menu')
+  //   .then(data => createCard(data))
+
+  
+    // function createCard(data) {
+    //   data.forEach(({img, altimg, title, descr, price}) => {
+
+    //     price = price * 27;
+
+    //     const element = document.createElement('div');
+
+    //     element.classList.add('menu__item');
+
+    //     element.innerHTML = `
+        
+    //     <img src=${img} alt=${altimg}>
+    //     <h3 class="menu__item-subtitle">${title}</h3>
+    //     <div class="menu__item-descr">${descr}</div>
+    //     <div class="menu__item-divider"></div>
+    //     <div class="menu__item-price">
+    //         <div class="menu__item-cost">Цена:</div>
+    //         <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //       </div>
+    //     `;
+    //     document.querySelector('.menu .container').append(element);
+    //   })
+    // }
+
+
+  
+
 // Forms
 
 const forms = document.querySelectorAll('form');
