f8ebf0db0b64c52697995d720862705dba285960
Kort <de.kort@yahoo.de>
2022-05-28 19:48:25 +0300

    com

--- src/js/main.js
+++ src/js/main.js
@@ -402,13 +402,16 @@ function showSlideIndex() {
 }
 }
 
+function deletNotDigits(str) {
+  return +str.replace(/\D/g, '');
+}
 
 next.addEventListener('click', () => {
 
-  if(offset == +width.slice(0, 3) * (slides.length - 1)) {
+  if(offset == deletNotDigits(width) * (slides.length - 1)) {
     offset = 0;
   } else {
-    offset += +width.slice(0, 3);
+    offset += deletNotDigits(width) ;
   }
 
   slidesInner.style.transform = `translateX(-${offset}px)`;
@@ -428,9 +431,9 @@ next.addEventListener('click', () => {
 prev.addEventListener('click', () => {
 
   if(offset == 0) {
-   offset = +width.slice(0, 3) * (slides.length - 1)
+   offset = deletNotDigits(width) * (slides.length - 1)
   } else {
-    offset -= +width.slice(0, 3);
+    offset -= deletNotDigits(width);
   }
 
   slidesInner.style.transform = `translateX(-${offset}px)`;
@@ -451,7 +454,7 @@ dotsArray.forEach(item => {
 
     slideIndex = dotAtr;
 
-    offset = +width.slice(0, 3) * (slideIndex - 1)
+    offset = deletNotDigits(width) * (slideIndex - 1)
     slidesInner.style.transform = `translateX(-${offset}px)`;
   
     showSlideIndex();
@@ -464,8 +467,6 @@ dotsArray.forEach(item => {
   
   })
 })
-
-
 
 // showSlides(setIndex)
 
@@ -512,6 +513,8 @@ dotsArray.forEach(item => {
 // next.addEventListener('click', () => {
 //   changeSliderIndex(1)
 // })
+
+
 
 
 
